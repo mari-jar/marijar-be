f90e5ab7fee978fd503378d0ac47f7ef7697055e
Mochamad Aufa <mochamadaufa7@gmail.com>
2022-06-12 22:00:50 +0700

    add migration table

--- migration/table_sql/commons.sql
+++ migration/table_sql/commons.sql
@@ -1,5 +1,4 @@
-CREATE TABLE IF NOT EXISTS commons
-(
+CREATE TABLE IF NOT EXISTS commons (
     id UUID NOT NULL PRIMARY KEY,
     type character varying(255) COLLATE pg_catalog."default" NOT NULL,
     data JSONB NOT NULL
