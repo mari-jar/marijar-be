584cfda0576c6a9521306f990171e95b11a46f1e
Mochamad Aufa <mochamadaufa7@gmail.com>
2022-06-24 18:54:00 +0700

    add query insert many & migration students

--- bin/repositories/employee/model/model.js
+++ bin/repositories/employee/model/model.js
@@ -1,5 +1,6 @@
 module.exports = {
   insertReq: require('./model_json/insert-req.json'),
+  insertManyReq: require('./model_json/insert-many-req.json'),
   updateReq: require('./model_json/update-req.json'),
   findOneRes: require('./model_json/find-one-res.json'),
   findManyRes: require('./model_json/find-many-res.json')
